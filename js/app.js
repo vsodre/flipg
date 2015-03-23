d76a335650c8623bfcc677bf4e9a95475575990b
vsodre <victors@id.uff.br>
2015-03-23 03:43:10 -0500

    search and tabs layout

--- js/app.js
+++ js/app.js
@@ -2,8 +2,19 @@
     var app = angular.module("mockup", ['ngTouch']);
     app.controller('DashboardController', ['$http', function (http) {
         var dashboard = this;
-        dashboard.feeds = [];
+        dashboard.activeFilter = 0;
         dashboard.menuState = false;
+        dashboard.filters = [{"name":"All"}, {"name":"Personal Filter 1"}, {"name":"Personal Filter 2"}, {"name":"Personal Filter 3"}];
+        dashboard.feeds = [];
+        dashboard.isActive = function(i){
+            return i == dashboard.activeFilter;
+        };
+        dashboard.activeFilterName = function(){
+            return dashboard.filters[dashboard.activeFilter].name;
+        };
+        dashboard.setActiveFilter = function(i){
+            dashboard.activeFilter = i;
+        };
         dashboard.toggleMenu = function(){
            dashboard.menuState = !dashboard.menuState; 
         };
