b9392f573a6184bb2532c53448cb45da85618009
vsodre <victors@id.uff.br>
2015-04-19 21:56:05 -0500

    Channel model added

---
+++ app/Channel.php
@@ -0,0 +1,8 @@
+<?php namespace App;
+
+use Jenssegers\Mongodb\Model;
+
+class Channel extends Model {
+
+	protected $collection = 'channels_collection';
+}
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit996bd90bbcc7fff1cd7c6d3a5b22a027::getLoader();
+return ComposerAutoloaderInit3ff54efbb6acae54ccde0fa679891110::getLoader();
